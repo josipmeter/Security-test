53fd7b540f07d15c52a3695d3851526c2e1c39b4
josipmeter <jm51178@fer.hr>
2022-11-06 00:07:09 +0100

    render config

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require("express")
 const path = require("path")
+const dotenv = require("dotenv")
 
 const app = express();
 
+dotenv.config()
+
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug")
+
+const externalUrl = process.env.RENDER_EXTERNAL_URL;
+const port = externalUrl && process.env.PORT ? parseInt(process.env.PORT) : 4080;
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: true}));
@@ -22,7 +28,13 @@ app.post('/query',  function (req, res) {
         res.redirect("/")
 });
 
-app.listen(3000, () => {
+if (externalUrl) {
+  const hostname = '127.0.0.1';
+  app.listen(port, hostname, () => {
+  console.log(`Server locally running at http://${hostname}:${port}/ and from outside on ${externalUrl}`);
+  });
+}
+else{
+  app.listen(3000, () => {
     console.log('The application is listening on port 3000!');
-})
-
+})}
