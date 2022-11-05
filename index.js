9349c068938ac3024dab54984b9e7e53e733f16e
josipmeter <jm51178@fer.hr>
2022-11-05 23:40:25 +0100

    Refactoring first part

--- index.js
+++ index.js
@@ -19,7 +19,6 @@ app.get("/", (req, res) => {
 app.post('/query',  function (req, res) {
         toDisplay = req.body.query
         checked = req.body.xssAttack
-        console.log(checked)
         res.redirect("/")
 });
 
@@ -27,8 +26,3 @@ app.listen(3000, () => {
     console.log('The application is listening on port 3000!');
 })
 
-function getQuery(){
-    let query = new URL(window.location).searchParams.get('query')
-    document.getElementById('query-input').value = query
-    document.getElementById('query-output').innerHTML = query
-}
