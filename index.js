ac52ec07f1398bffa12219571f0dafa4486bd3a2
josipmeter <jm51178@fer.hr>
2022-11-11 14:37:33 +0100

    bac attack

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 const express = require("express")
 const path = require("path")
 const dotenv = require("dotenv")
-
+const ESAPI = require('node-esapi');
 const app = express();
+const { auth } = require('express-openid-connect');
 
 dotenv.config()
 
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: 'a long, randomly-generated string stored in env',
+  baseURL: 'http://localhost:3000',
+  clientID: 'vrel4nRkW6gaJONGdK4RF53zQXpbwfrV',
+  issuerBaseURL: 'https://dev-rnrkynepkbpmq6qo.us.auth0.com'
+};
+
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug")
+app.use(auth(config));
 
 const externalUrl = process.env.RENDER_EXTERNAL_URL;
 const port = externalUrl && process.env.PORT ? parseInt(process.env.PORT) : 4080;
@@ -16,17 +27,32 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: true}));
 
 let toDisplay = ""
-let checked = ""
+let checkedXss = ""
+let checkedBac = { name: "" };
 
 app.get("/", (req, res) => {
-    res.render('index',{toDisplay, checked})
+    res.render('index',{toDisplay, checkedXss})
 })
 
 app.post('/query',  function (req, res) {
         toDisplay = req.body.query
-        checked = req.body.xssAttack
+        checkedXss = req.body.xssAttack
+        checkedBac.name = req.body.bacAttack
         res.redirect("/")
 });
+
+app.get("/logged", (req, res) => {
+  if(checkedBac.name==="bacAttack" || req.oidc.isAuthenticated()){
+    res.render('logged')
+  }
+  else{
+    res.status(401);
+    res.send('You are not authorized');
+  }
+})
+
+
+
 
 if (externalUrl) {
   const hostname = '127.0.0.1';
